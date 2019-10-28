c3de6d82f3d5f0a6b0cdacc857e0a397af3c173d
TheAvaro <TheAvaro@users.noreply.github.com>
2019-10-28 01:26:32 +0100

    Reiniciado escena y scripts
    Movimiento básico y camara que funciona regular.

---
+++ RoboPinballUnity/Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector2 axis;
+	private Rigidbody rb;
+	private Vector3 movement;
+
+	public float speed;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate ()
+	{
+		//Movimiento
+		
+		axis.x = Input.GetAxis("Horizontal");														
+		axis.y = Input.GetAxis("Vertical");
+
+		movement = new Vector3 (axis.x, 0.0f, axis.y);
+
+		rb.AddForce(movement * speed);
+	}
+}
